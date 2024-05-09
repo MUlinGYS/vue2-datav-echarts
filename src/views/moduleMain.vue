371759170e6752b982b3756195504e824eca1979
kaichen <3069394597@qq.com>
2024-05-09 17:36:37 +0800

    纠错

--- src/views/moduleMain.vue
+++ src/views/moduleMain.vue
@@ -1,63 +1,57 @@
 <template>
-<div class="module-box" style="height:58%">
-				<div style="flex: 0 1 25%">
-					<dv-border-box-13 style="width: 100%; height: 33.33%" 
-						><capsule :dataChart="dataChart" :colorsChart="colorsChart" style="padding:20px 10px 0 20px"/></dv-border-box-13>
-						<dv-water-level-pond :config="config3" style="width: 100%; height: 33.33%;" />
-					<dv-border-box-9 style="width: 100%; height: 33.33%"
-						><dv-conical-column-chart :config="config5" style="width:90%;height:90%;margin-top:10px" /></dv-border-box-9
-					>
-				</div>
-				<div style="flex: 0 1 50%">
-					<dv-border-box-12 style="width: 100%; height: 100%"
-						><dv-flyline-chart-enhanced :config="config4" style="width:100%;height:100%;" /></dv-border-box-12
-					>
-				</div>
-				<div style="flex: 0 1 25%">
-					<dv-border-box-1 style="width: 100%; height: 50%;"
-						><dv-capsule-chart
-							:config="config2"
-							style="width: 90%; height: 90%;padding:20px 10px 0 20px" 
-					/></dv-border-box-13></dv-border-box-1
-					>
-					<dv-border-box-8 style="width: 100%; height: 50%"
-						><dv-scroll-ranking-board :config="config6" style="width:90%;height:100%" /></dv-border-box-8
-					>
-				</div>
-			</div>
+  <div class="module-box" style="height:58%">
+    <div style="flex: 0 1 25%">
+      <dv-border-box-13 style="width: 100%; height: 33.33%">
+        <capsule :dataChart="dataChart" :colorsChart="colorsChart" style="padding:20px 10px 0 20px" />
+      </dv-border-box-13>
+      <dv-water-level-pond :config="config3" style="width: 100%; height: 33.33%;" />
+      <dv-border-box-9 style="width: 100%; height: 33.33%"><dv-conical-column-chart :config="config5"
+          style="width:90%;height:90%;margin-top:10px" /></dv-border-box-9>
+    </div>
+    <div style="flex: 0 1 50%">
+      <dv-border-box-12 style="width: 100%; height: 100%"><dv-flyline-chart-enhanced :config="config4"
+          style="width:100%;height:100%;" /></dv-border-box-12>
+    </div>
+    <div style="flex: 0 1 25%">
+      <dv-border-box-1 style="width: 100%; height: 50%;"><dv-capsule-chart :config="config2"
+          style="width: 90%; height: 90%;padding:20px 10px 0 20px" /></dv-border-box-1>
+      <dv-border-box-8 style="width: 100%; height: 50%"><dv-scroll-ranking-board :config="config6"
+          style="width:90%;height:100%" /></dv-border-box-8>
+    </div>
+  </div>
 </template>
 <script>
 import capsule from './capsule.vue'
 
 export default {
-  name:'moduleMain',
+  name: 'moduleMain',
   components: {
     capsule
   },
-  data () {
+  data() {
     return {
-      dataChart:[
+      dataChart: [
         {
           name: "RO1",
           value: 1020,
-        },        {
+        }, {
           name: "RO2",
           value: 1300,
-        },        {
+        }, {
           name: "RO3",
           value: 2000,
-        },        {
+        }, {
           name: "RO4",
           value: 1300,
-        },        {
+        }, {
           name: "RO5",
           value: 1004,
-        },        {
+        }, {
           name: "RO6",
           value: 1020,
         }
       ],
-      colorsChart:["#e062ae", "#fb7293", "#e690d1", "#32c5e9", "#96bfff"],
+      colorsChart: ["#e062ae", "#fb7293", "#e690d1", "#32c5e9", "#96bfff"],
       config2: {
         data: [
           {
@@ -250,87 +244,87 @@ export default {
           src: require("../assets/img/mapPoint.png"),
         },
       },
-      config5:{
-         data: [
-    {
-      name: '周口',
-      value: 55
-    },
-    {
-      name: '南阳',
-      value: 120
-    },
-    {
-      name: '西峡',
-      value: 71
-    },
-    {
-      name: '驻马店',
-      value: 66
-    },
-    {
-      name: '新乡',
-      value: 80
-    },
-    {
-      name: '信阳',
-      value: 35
-    },
-    {
-      name: '漯河',
-      value: 15
-    }
-  ],
-  img: [
-    require('../assets/img/1st.png'),
-    require('../assets/img//2st.png'),
-    require('../assets/img//3st.png'),
-    require('../assets/img//4st.png'),
-    require('../assets/img//5st.png'),
-    require('../assets/img//6st.png'),
-    require('../assets/img//7st.png')
-  ],
-  showValue: true
+      config5: {
+        data: [
+          {
+            name: '周口',
+            value: 55
+          },
+          {
+            name: '南阳',
+            value: 120
+          },
+          {
+            name: '西峡',
+            value: 71
+          },
+          {
+            name: '驻马店',
+            value: 66
+          },
+          {
+            name: '新乡',
+            value: 80
+          },
+          {
+            name: '信阳',
+            value: 35
+          },
+          {
+            name: '漯河',
+            value: 15
+          }
+        ],
+        img: [
+          require('../assets/img/1st.png'),
+          require('../assets/img//2st.png'),
+          require('../assets/img//3st.png'),
+          require('../assets/img//4st.png'),
+          require('../assets/img//5st.png'),
+          require('../assets/img//6st.png'),
+          require('../assets/img//7st.png')
+        ],
+        showValue: true
       },
-      config6:{
-          data: [
-    {
-      name: '周口',
-      value: 55
-    },
-    {
-      name: '南阳',
-      value: 120
-    },
-    {
-      name: '西峡',
-      value: 78
-    },
-    {
-      name: '驻马店',
-      value: 66
-    },
-    {
-      name: '新乡',
-      value: 80
-    },
-    {
-      name: '信阳',
-      value: 45
-    },
-    {
-      name: '漯河',
-      value: 29
-    }
-  ]
+      config6: {
+        data: [
+          {
+            name: '周口',
+            value: 55
+          },
+          {
+            name: '南阳',
+            value: 120
+          },
+          {
+            name: '西峡',
+            value: 78
+          },
+          {
+            name: '驻马店',
+            value: 66
+          },
+          {
+            name: '新乡',
+            value: 80
+          },
+          {
+            name: '信阳',
+            value: 45
+          },
+          {
+            name: '漯河',
+            value: 29
+          }
+        ]
       }
+    }
   }
-}
 }
 </script>
 
 <style scoped>
-::v-deep .border-box-content{
+::v-deep .border-box-content {
   display: flex !important;
   justify-content: center !important;
   align-content: center !important;
